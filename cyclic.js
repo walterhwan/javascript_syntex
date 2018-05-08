00b72cff2a3eeffb99ee90f5761bf4e5cd825842
Walter Wan <WalterHWan@gmail.com>
2018-05-08 09:08:06 -0700

    Finish cyclic.js

--- cyclic.js
+++ cyclic.js
@@ -22,15 +22,59 @@ function isCyclic(rootNode) {
   }
 }
 
+function length(node) {
+  if (isCyclic(node)) {
+    return null;
+  }
+
+  let len = 0;
+  while (node) {
+    node = node.next;
+    len += 1;
+  }
+
+  return len;
+}
+
+function willConverge(a, b) {
+  let difference = length(a) - length(b);
+  let aRunner = a;
+  let bRunner = b;
+
+  if (difference > 0) {
+    for (let i = 0; i < difference; i++) {
+      aRunner = aRunner.next;
+    }
+  } else {
+    for (let i = 0; i < -difference; i++) {
+      bRunner = bRunner.next;
+    }
+  }
+
+  while (aRunner) {
+    if (aRunner === bRunner) return true;
+    aRunner = aRunner.next;
+    bRunner = bRunner.next;
+  }
+
+  return false;
+}
 
 let a = new Node('A');
 let b = new Node('B');
 let c = new Node('C');
-a.next = b;
-b.next = c;
-c.next = a;
+let d = new Node('D');
+let e = new Node('E');
+let f = new Node('F');
+a.next = c;
+c.next = e;
+b.next = e;
+e.next = f;
 
 console.log(isCyclic(a));
+// console.log(length(a));
+// console.log(length(b));
+console.log(willConverge(a, b));
 
 // console.log('foo' === 'foo');
 // console.log(new String('foo') === new String('foo'));
