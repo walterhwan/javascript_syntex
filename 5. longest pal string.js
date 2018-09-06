0a142240a3b483c3cbfab6ba6d198307c1782f95
Walter Wan <WalterHWan@gmail.com>
2018-09-06 15:46:34 -0700

    Add 5

--- 5. longest pal string.js
+++ 5. longest pal string.js
@@ -1,47 +1,82 @@
 var longestPalindrome = function(string) {
-  let memory = {};
-  let length = 0;
-  let longestPal = "";
+  // let memory = {};
+  // let length = -1;
+  // let palPos = [0, 0];
+  // for (let i = 0; i < string.length; i++) {
+  //   for (let j = i; j < string.length; j++) {
+  //     let word = string.slice(i, j + 1);
+  //     // console.log("i, j = %s %s", i, j);
+  //     let len = j - i;
+  //     // console.log("word:%s", string.slice(i, j + 1));
+  //     if (memory[word] && string.charAt(i) === string.charAt(j - 1)) {
+  //       memory[word] = true;
+  //       if (length < len) {
+  //         length = len;
+  //         palPos = [i, j + 1];
+  //       }
+  //       continue;
+  //     }
+  //     if (isPal(i, j, string)) {
+  //       memory[word] = true;
+  //       if (length < len) {
+  //         length = len;
+  //         palPos = [i, j + 1];
+  //       }
+  //     } else {
+  //       memory[word] = false;
+  //     }
+  //   }
+  // }
 
+  // // console.log(memory);
+  // return string.slice(...palPos);
+  if (string === null || string.length < 1) return "";
+  let start = 0,
+    end = 0;
   for (let i = 0; i < string.length; i++) {
-    for (let j = i + 1; j < string.length + 1; j++) {
-      let word = string.slice(i, j);
-      console.log("word:%s", word);
-      if (memory[[i + 1, j - 2]] && string.charAt(i) === string.charAt(j - 1)) {
-        console.log("yes");
-        memory[[i, j]] = true;
-        if (length < word.length) {
-          length = word.length;
-          longestPal = word;
-        }
-        continue;
-      }
-      if (isPal(word)) {
-        memory[[i, j]] = true;
-        if (length < word.length) {
-          length = word.length;
-          longestPal = word;
-        }
-      } else {
-        memory[[i, j]] = false;
-      }
+    let len1 = lenOfPalFromCenter(string, i, i);
+    let len2 = lenOfPalFromCenter(string, i, i + 1);
+    let len = Math.max(len1, len2);
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
     }
   }
-
-  console.log(memory);
-
-  return longestPal;
+  return string.slice(start, end + 1);
 };
 
-var isPal = function(word) {
-  for (let i = 0; i < word.length / 2; i++) {
-    if (word.charAt(i) !== word.charAt(word.length - 1 - i)) {
-      return false;
-    }
+var lenOfPalFromCenter = function(string, left, right) {
+  let L = left,
+    R = right;
+
+  while (L >= 0 && R < string.length && string[L] === string[R]) {
+    L--;
+    R++;
   }
 
-  return true;
+  return R - L - 1;
 };
 
-let string = "erhannahweew";
+// var isPal = function(start, end, word) {
+//   let len = end - start + 1;
+//   if (start === end) return true;
+//   // let slice = word.slice(start, end + 1);
+//   let j = Math.floor(len / 2) + start,
+//     i = j;
+//   if (len % 2 === 0) {
+//     i = j - 1;
+//   }
+
+//   while (i >= start && j <= end) {
+//     if (word[i] != word[j]) {
+//       return false;
+//     }
+//     i--;
+//     j++;
+//   }
+
+//   return true;
+// };
+
+let string = "babadhannaheio";
 console.log(longestPalindrome(string));
